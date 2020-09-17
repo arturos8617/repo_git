846fd36ac3290be9926aa8fdbc61b28e96a89a4c
Arturo Sanchez <arturo.sanchez7480@alumnos.udg.mx>
2020-09-16 23:52:30 -0500

    capturar edad y mostrar la edad mas uno

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 main(){
 	char name[50];
+	unsigned int age;
 	printf("Hola Mundo\n");
 	printf("Escribe tu nombre");
 	fgets(name, sizeof(name),stdin);
 	printf("Bienvenido %s",name);
+	printf("Ingresa Tu Edad");
+	scanf("%d",&age);
+	printf("Vas a cumplir %d",age+1);
 	return 0;
 }
