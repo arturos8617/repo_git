0bb3b80895618b6760c937bc6ed7326a03d6739b
Arturo Sanchez <arturo.sanchez7480@alumnos.udg.mx>
2020-09-16 23:33:01 -0500

    captura de nombre completo usando fgets

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 main(){
+	char name[50];
 	printf("Hola Mundo\n");
+	printf("Escribe tu nombre");
+	fgets(name, sizeof(name),stdin);
+	printf("Bienvenido %s",name);
 	return 0;
 }
