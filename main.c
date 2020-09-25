741430372cd845e94d17895c18a21b81c2e6f940
Arturo Sanchez <arturo.sanchez7480@alumnos.udg.mx>
2020-09-25 08:47:09 -0500

    Comentamos hola mundo

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 main(){
 	char name[50];
 	unsigned int age;
-	printf("Hola Mundo\n");
+	//printf("Hola Mundo\n");
 	printf("Escribe tu nombre");
 	fgets(name, sizeof(name),stdin);
 	printf("Bienvenido %s",name);
